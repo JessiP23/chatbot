ca1248186131c75cad31c7ab020279cf98312e89
JessiP23 <jessi316866@gmail.com>
2024-08-05 20:25:26 -0400

    response from the server successfull

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -3,8 +3,9 @@ import { NextResponse } from "next/server"
 
 // initial request
 
-export function POST(req) {
-    console.log('POST /api/chat')
+export async function POST(req) {
+    const data = await req.json()
+    console.log(data);
     return NextResponse.json({message: "Hello from the server!"})
 }
 
